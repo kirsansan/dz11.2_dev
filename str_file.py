908684246eb3c35f1170a9ce5de416c006ca75cb
kirsansan <kirill_home@mail.ru>
2023-03-26 18:59:27 +0300

    add make_title func

--- str_file.py
+++ str_file.py
@@ -2,8 +2,20 @@ def make_upper(my_string: str) -> str:
     return my_string.upper()
 
 
+def make_title(my_string: str) -> str:
+    """make title for all words in string"""
+    tmp_str = None
+    for item in my_string.split(" "):
+        if tmp_str:
+            tmp_str = tmp_str + " " + item.title()
+        else:
+            tmp_str = item.title()
+    return tmp_str
+
+
 def test():
     print(make_upper("qwerty"))
+    print(make_title("obey your master (c) metallica"))
 
 
 if __name__ == '__main__':
